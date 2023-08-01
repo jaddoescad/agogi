487daaa8d5912ec5e55f38ee37d305cb43c5ff0b
Jad Slim <jadslimm@gmail.com>
2023-08-01 18:25:50 -0400

    init my quizzes

--- pages/myquizzes.tsx
+++ pages/myquizzes.tsx
@@ -4,8 +4,21 @@ import Link from 'next/link';
 import React from 'react';
 import { IoMdCheckmarkCircleOutline } from 'react-icons/io';
 import { useState, useEffect } from 'react';
-import { Box, VStack, HStack, Text, Button, Image, Center, Flex } from '@chakra-ui/react';
+import {
+  Box,
+  Grid,
+  VStack,
+  HStack,
+  Text,
+  Button,
+  Image,
+  Center,
+  Flex,
+  SimpleGrid
+} from '@chakra-ui/react';
 import { CheckCircleIcon } from '@chakra-ui/icons';
+import { Link as ChakraLink } from '@chakra-ui/react';
+import { QuizCard } from 'components/QuizCards/QuizCard';
 
 type Quiz = {
   id: number;
@@ -18,7 +31,7 @@ const MyQuizzes: React.FC = () => {
 
   useEffect(() => {
     const fetchData = async () => {
-      const result = await getMyQuizzes(); 
+      const result = await getMyQuizzes();
       setQuizzes(result);
     };
 
@@ -27,65 +40,33 @@ const MyQuizzes: React.FC = () => {
 
   return (
     <Box pb={10}>
-      <Flex minH="screen" direction="column" alignItems="center" textColor="black">
+      <Box minH="screen" alignItems="center">
         <Head>
-          <title>My Quizzes</title>
+          <title>Quiz App</title>
         </Head>
 
-        <Text fontSize="4xl" fontWeight="bold" mt={4}>My Quizzes</Text>
-
-        <Flex direction="column" alignItems="center" justifyContent="center" mt={10} maxW="6xl" fontWeight="semibold">
-          {quizzes.map((quiz) => (
-            <Link href={`/quiz/${quiz.id}`}>
-              <Flex
-                key={quiz.id}
-                m={2}
-                h="64"
-                w="64"
-                direction="column"
-                alignItems="center"
-                justifyContent="space-between"
-                borderRadius="3xl"
-                p={5}
-                bg="#162457"
-                color="white"
-                _hover={{cursor: "pointer"}}
-              >
-                <Box
-                  fontWeight="bold"
-                  mb={2}
-                  w="full"
-                  rounded="full"
-                  p={2}
-                  textAlign="center"
-                  bg="green"
-                >
-                  True or False
-                </Box>
-
-                <Text mb={2} textAlign="center" fontSize="lg">
-                  {quiz.title}
-                </Text>
-
-                <Flex w="full" justifyContent="space-between" fontSize="sm">
-                  <Box
-                    p={2}
-                    py={1}
-                    rounded="full"
-                    bg="red"
-                  >
-                    {quiz.difficulty}
-                  </Box>
-                  <Flex alignItems="center" bg="blue.800" p={2} py={1} rounded="full">
-                    <CheckCircleIcon mr={2} />
-                    {0}
-                  </Flex>
-                </Flex>
-              </Flex>
-            </Link>
-          ))}
-        </Flex>
-      </Flex>
+        <Box alignItems="center" justifyContent="center" w="full">
+          <Center>
+            <Text fontSize={['3xl', '5xl']}>My Quizzes</Text>
+          </Center>
+          <Box
+            mt={10}
+            maxW="6xl"
+            mx="auto"
+            textAlign="center"
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+            justifyContent="center"
+          >
+            <SimpleGrid columns={[1, 1, 2, 3, 3]}>
+              {quizzes.map((quiz) => (
+                <QuizCard quiz={quiz} key={quiz.id} />
+              ))}
+            </SimpleGrid>
+          </Box>
+        </Box>
+      </Box>
     </Box>
   );
 };
