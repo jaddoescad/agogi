1cbcbe2a27a40ab0fff42ffa790c51689f74f73f
Jad Slim <jadslimm@gmail.com>
2023-08-01 17:00:50 -0400

    rfmkrf

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -41,7 +41,7 @@ const SignIn = () => {
               //       brandAccent: 'lightgreen'
               //     }
               //   }
-              }
+              // }
             }}
           />
        </Box>
