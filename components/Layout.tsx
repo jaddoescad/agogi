2b02072bd3ea1ef8444cebd38f20405bfa9775b8
Jad Slim <jadslimm@gmail.com>
2023-08-02 15:37:42 -0400

    removed og image

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -46,7 +46,6 @@ export default function Layout({ children, meta: pageMeta }: Props) {
         <meta property="og:site_name" content={meta.title} />
         <meta property="og:description" content={meta.description} />
         <meta property="og:title" content={meta.title} />
-        <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@vercel" />
         <meta name="twitter:title" content={meta.title} />
         <meta name="twitter:description" content={meta.description} />
