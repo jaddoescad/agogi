1d1dbf8b8137da4d8bb7d6feedff9890a9920dc1
Jad Slim <jadslimm@gmail.com>
2023-08-02 15:16:00 -0400

    modified header

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -19,8 +19,7 @@ export default function Layout({ children, meta: pageMeta }: Props) {
   const router = useRouter();
   const meta = {
     title: 'Agogi',
-    description: 'AI powered quiz platform',
-    cardImage: '/og.png',
+    description: 'AI powered quiz platform'
     ...pageMeta
   };
 
