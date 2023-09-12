3a878eb9314506d5765f7826a23ff1ab6cb77496
Jad Slim <jadslimm@gmail.com>
2023-09-12 17:27:49 -0400

    test

--- pages/api/book-response.ts
+++ pages/api/book-response.ts
@@ -17,91 +17,103 @@ import {
 } from 'prompts/book-generator/subprompts/check-message-type';
 
 const apiHandler = async (req: NextApiRequest, res: NextApiResponse) => {
-  console.log("gejjnefnrjnrjfnrfjnrfjnrjfnrfjnrj")
   res.status(200).json({ name: 'John Doe' });
 
-  // if (req.method === 'POST') {
-  //   try {
-  //     const supabaseServerClient = createServerSupabaseClient({ req, res });
+  if (req.method === 'POST') {
+    try {
+      const supabaseServerClient = createServerSupabaseClient({ req, res });
+      console.log('supabase server initialized');
 
-  //     const { message, quizId, quizType, topicId } =
-  //       (await req.body) as RequestData;
+      const { message, quizId, quizType, topicId } =
+        (await req.body) as RequestData;
 
-  //     await saveMessage(supabaseServerClient, message, topicId, 'user');
+      await saveMessage(supabaseServerClient, message, topicId, 'user');
 
-  //     const llmGpt4 = new OpenAI({
-  //       openAIApiKey: process.env.OPENAI_API_KEY,
-  //       temperature: 0.7,
-  //       modelName: 'gpt-3.5-turbo-16k'
-  //     });
+      console.log('message saved');
 
-  //     const prompt = quizIntentionPrompt(message);
-  //     const result = await llmGpt4.predict(prompt);
+      const llmGpt4 = new OpenAI({
+        openAIApiKey: process.env.OPENAI_API_KEY,
+        temperature: 0.7,
+        modelName: 'gpt-3.5-turbo-16k'
+      });
 
+      const prompt = quizIntentionPrompt(message);
+      const result = await llmGpt4.predict(prompt);
 
-  //     // Parsing the response
-  //     const parsedResponse = JSON.parse(result);
-
-  //     const aiResponse = parsedResponse.ai_response;
-  //     const promptQuestions = parsedResponse.questions;
+      console.log('result', result);
 
 
-  //     const questionsPromises: Promise<any>[] = [];
+      // Parsing the response
+      const parsedResponse = JSON.parse(result);
 
-  //     // Looping through the questions to get answers from AI and store them in questionsArray
-  //     for (const question of promptQuestions) {
-  //       const questionPrompt_ = questionPrompt(question);
-  //       const questionPromise = llmGpt4
-  //         .predict(questionPrompt_)
-  //         .then((result) => {
-  //           const questionResponse = JSON.parse(result); // Assuming your questionPrompt expects a structured JSON response
-  //           return questionResponse; // You might need to adjust this if the structure is nested
-  //         });
-  //       questionsPromises.push(questionPromise);
-  //     }
-
-  //     // Wait for all the questions to be processed
-  //     const questionsArray = await Promise.all(questionsPromises);
-
-  //     //LOOP THROU
-  //       await insertQuestions(
-  //         supabaseServerClient,
-  //         questionsArray,
-  //         topicId,
-  //         quizType
-  //       );
+      const aiResponse = parsedResponse.ai_response;
+      const promptQuestions = parsedResponse.questions;
 
 
-  //     if (aiResponse) {
-  //       await saveMessage(
-  //         supabaseServerClient,
-  //         aiResponse,
-  //         topicId,
-  //         'ai'
-  //       );
-  //     }
+      const questionsPromises: Promise<any>[] = [];
+
+      // Looping through the questions to get answers from AI and store them in questionsArray
+      for (const question of promptQuestions) {
+        const questionPrompt_ = questionPrompt(question);
+        const questionPromise = llmGpt4
+          .predict(questionPrompt_)
+          .then((result) => {
+            const questionResponse = JSON.parse(result); // Assuming your questionPrompt expects a structured JSON response
+            return questionResponse; // You might need to adjust this if the structure is nested
+          });
+        questionsPromises.push(questionPromise);
+      }
+
+      // Wait for all the questions to be processed
+      const questionsArray = await Promise.all(questionsPromises);
+
+      console.log('questionsArray', questionsArray);
+
+      //LOOP THROU
+        await insertQuestions(
+          supabaseServerClient,
+          questionsArray,
+          topicId,
+          quizType
+        );
+
+        console.log('questions inserted');
+
+
+      if (aiResponse) {
+        await saveMessage(
+          supabaseServerClient,
+          aiResponse,
+          topicId,
+          'ai'
+        );
+      }
+
+      console.log('ai response saved');
 
 
 
-  //     // Structuring the final response
-  //     const response = {
-  //       message: aiResponse,
-  //       questions: questionsArray
-  //     };
+      // Structuring the final response
+      const response = {
+        message: aiResponse,
+        questions: questionsArray
+      };
 
-  //     return res.status(200).json(response);
-  //   } catch (error: any) {
-  //     console.error(error);
-  //     res.status(500).json({
-  //       error: {
-  //         message: error?.message ?? 'Internal Server Error'
-  //       }
-  //     });
-  //   }
-  // } else {
-  //   res.setHeader('Allow', 'POST');
-  //   res.status(405).end('Method Not Allowed');
-  // }
+      console.log('response', response);
+
+      return res.status(200).json(response);
+    } catch (error: any) {
+      console.error(error);
+      res.status(500).json({
+        error: {
+          message: error?.message ?? 'Internal Server Error'
+        }
+      });
+    }
+  } else {
+    res.setHeader('Allow', 'POST');
+    res.status(405).end('Method Not Allowed');
+  }
 };
 
 export default apiHandler;
